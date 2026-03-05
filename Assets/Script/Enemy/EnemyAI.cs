c69703c4ed8c817fd147254ea56476bf760a1aec
Zeprhy <lianradith@gmail.com>
2026-03-05 20:41:03 +0700

    Revert "add path finding"
    This reverts commit 4cefe2fcd97500e2a0f86fa31617950b1dd87182.

--- Assets/Script/Enemy/EnemyAI.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using UnityEngine.AI;
-using System.Collections.Generic;
-
-public class EnemyAI : MonoBehaviour
-{
-    public enum EnemyState { Patrolling, Chasing, Attacking}
-}
