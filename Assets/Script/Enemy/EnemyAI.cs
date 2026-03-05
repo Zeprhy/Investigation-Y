99a978c28488fff60a843c4ab800968bfcfdf7e5
Zeprhy <lianradith@gmail.com>
2026-03-05 20:42:48 +0700

    Reapply "add path finding"
    This reverts commit c69703c4ed8c817fd147254ea56476bf760a1aec.

---
+++ Assets/Script/Enemy/EnemyAI.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections.Generic;
+
+public class EnemyAI : MonoBehaviour
+{
+    public enum EnemyState { Patrolling, Chasing, Attacking}
+}
