454d2042208f6d3a83dcac4948c1c9dad8634db5
Zeprhy <lianradith@gmail.com>
2026-03-15 12:30:26 +0700

    Animation
    Idle,Investigation,Run,Attack,Walk

--- Assets/Script/Enemy/EnemyAnimationController.cs
+++ Assets/Script/Enemy/EnemyAnimationController.cs
@@ -5,16 +5,32 @@ public class EnemyAnimationController : MonoBehaviour
 {
     private NavMeshAgent agent;
     private Animator anim;
+    private EnemyAI enemyAI;
+
+    private EnemyAI.EnemyState previousState;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         anim = GetComponentInChildren<Animator>();
+        enemyAI = GetComponent<EnemyAI>();
+        previousState = enemyAI.currentState;
     }
 
     void Update()
     {
+        if( enemyAI == null) return;
         float currentSpeed = agent.velocity.magnitude;
+        EnemyAI.EnemyState state = enemyAI.currentState;
+
         anim.SetFloat("Speed", currentSpeed);
+
+        if (state == EnemyAI.EnemyState.Attacking && previousState != EnemyAI.EnemyState.Attacking)
+        {
+           anim.SetTrigger("Attack");
+        }
+        
+        anim.SetBool("Investigating", state == EnemyAI.EnemyState.Investigating);
+        previousState = state;
     }
 }
